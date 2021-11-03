4ea234d1a76d2191ff1c91ae31e2695c3f124cb7
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-11-03 14:04:23 +0100

    v0.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # setup
 setup(
     name='pyobs-aravis',
-    version='0.1',
+    version='0.14',
     description='pyobs component for the Aravis cameras',
     packages=['pyobs_aravis']
 )
