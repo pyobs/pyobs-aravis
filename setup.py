d48c1dc06f5e060ed7cd90857018c12978dfe89b
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-11-22 14:11:29 +0100

    poetry and github action

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-# setup
-setup(
-    name='pyobs-aravis',
-    version='0.14',
-    description='pyobs component for the Aravis cameras',
-    packages=['pyobs_aravis']
-)
