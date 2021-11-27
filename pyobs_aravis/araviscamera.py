920c579d3312f7bd5de89c8d87eba37deccbb0ef
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-11-27 18:12:19 +0100

    implemented sleep mechanism for video devices

--- pyobs_aravis/araviscamera.py
+++ pyobs_aravis/araviscamera.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 import time
 from typing import Any, Dict, Optional, List
 
@@ -27,6 +28,7 @@ class AravisCamera(BaseVideo, IExposureTime):
         self._device_name = device
         self._camera: Optional[aravis.Camera] = None
         self._settings: Dict[str, Any] = {} if settings is None else settings
+        self._camera_lock = threading.Lock()
 
         # thread
         if device is not None:
@@ -51,42 +53,63 @@ class AravisCamera(BaseVideo, IExposureTime):
         BaseVideo.close(self)
         self._close_camera()
 
-    def _open_camera(self):
-        # is open?
+    def _open_camera(self) -> None:
+        """Open camera."""
+
+        with self._camera_lock:
+            # is open?
+            self._close_camera()
+
+            # open camera
+            log.info('Connecting to camera %s...', self._device_name)
+            self._camera = aravis.Camera(self._device_name)
+            log.info('Connected.')
+
+            # settings
+            for key, value in self._settings.items():
+                log.info(f'Setting value {key}={value}...')
+                self._camera.set_feature(key, value)
+
+            # start acquisition
+            self._camera.start_acquisition_continuous(nb_buffers=5)
+
+    def _close_camera(self) -> None:
+        """Close camera."""
+        # stop camera
+        with self._camera_lock:
+            if self._camera is not None:
+                log.info('Closing camera...')
+                self._camera.stop_acquisition()
+                self._camera.shutdown()
+            self._camera = None
+
+    def _activate_camera(self) -> None:
+        """Can be overridden by derived class to implement inactivity sleep"""
+        self._open_camera()
+
+    def _deactivate_camera(self) -> None:
+        """Can be overridden by derived class to implement inactivity sleep"""
         self._close_camera()
 
-        # open camera
-        log.info('Connecting to camera %s...', self._device_name)
-        self._camera = aravis.Camera(self._device_name)
-        log.info('Connected.')
-
-        # settings
-        for key, value in self._settings.items():
-            log.info(f'Setting value {key}={value}...')
-            self._camera.set_feature(key, value)
-
-    def _close_camera(self):
-        # stop camera
-        if self._camera is not None:
-            log.info('Closing camera...')
-            self._camera.stop_acquisition()
-            self._camera.shutdown()
-        self._camera = None
-
     def _capture(self) -> None:
-        # start acquisition
-        self._camera.start_acquisition_continuous(nb_buffers=5)
+        """Take new images in loop."""
 
         # loop until closing
         last = time.time()
         while not self.closing.is_set():
-            # read frame
-            frame = self._camera.pop_frame()
+            # no camera or not active?
+            if self._camera is None or not self.camera_active:
+                # wait a little
+                self.closing.wait(0.1)
+                continue
 
             # if time since last image is too short, wait a little
             if time.time() - last < self._interval:
                 self.closing.wait(0.01)
                 continue
+
+            # read frame
+            frame = self._camera.pop_frame()
             last = time.time()
 
             # process it
